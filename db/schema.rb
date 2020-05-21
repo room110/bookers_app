95878c2dc73f7d5610e8f09e83385d8ca2e8c853
yukiito <ito.room.room.110@gmail.com>
2020-05-21 19:11:16 +0900

    [add]

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_05_20_154300) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
