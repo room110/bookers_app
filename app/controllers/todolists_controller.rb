64be9590f6c6f4c77024c7440211fb95a97aacea
yukiito <ito.room.room.110@gmail.com>
2020-05-25 22:16:27 +0900

    [Finish]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,33 +1,33 @@
 class TodolistsController < ApplicationController
-  def new
-  	  @book = Book.new
-  end
 
   def create
   	  book = Book.new(book_params)
       book.save
       redirect_to todolist_path(book.id) #詳細画面へリダイレクト
-
   end
 
   def index
   	  @books = Book.all
-  	
+  	  @book = Book.new
   end
+
 
   def show
   	  @book = Book.find(params[:id])
   end
 
+
   def edit
   	  @book = Book.find(params[:id])
   end
+
 
   def update
   	  book = Book.find(params[:id])
   	  book.update(book_params)
   	  redirect_to todolist_path(book.id)
   end
+
 
   def destroy
       book = Book.find(params[:id])
